511674160bfc7ec2e28ff4d851ca69fa377e7a6b
rustemRA <rustem_pimp@mail.ru>
2018-03-26 10:16:54 +0600

    Добавьте файлы проекта.

---
+++ лабка1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace лабка1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Location = new System.Drawing.Point(29, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(328, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(29, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(175, 165);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Click += new System.EventHandler(this.textBox5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(328, 165);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Click += new System.EventHandler(this.textBox6_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(29, 291);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.Click += new System.EventHandler(this.textBox7_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(175, 291);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.Click += new System.EventHandler(this.textBox8_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(328, 291);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.Click += new System.EventHandler(this.textBox9_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(328, 101);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 22);
+            this.textBox10.TabIndex = 9;
+            this.textBox10.Click += new System.EventHandler(this.textBox10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 397);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ лабка1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace лабка1
+{public partial class Form1 : Form
+    {public Form1()
+        { InitializeComponent();}
+private void textBox1_Click_1(object sender, EventArgs e)
+        { textBox1.Text = "номер 1";
+          textBox1.BackColor = System.Drawing.Color.Gold; }
+private void textBox2_Click(object sender, EventArgs e)
+        { textBox2.Text = "номер 2";
+           textBox2.BackColor = System.Drawing.Color.Gold;}
+private void textBox3_Click(object sender, EventArgs e)
+        {textBox3.Text = "номер 3";
+         textBox3.BackColor = System.Drawing.Color.Gold;}
+private void textBox4_Click(object sender, EventArgs e)
+        {textBox4.Text = "номер 4";
+         textBox4.BackColor = System.Drawing.Color.Gold;}
+private void textBox5_Click(object sender, EventArgs e)
+        { textBox5.Text = "номер 5";
+          textBox5.BackColor = System.Drawing.Color.Gold;}
+private void textBox6_Click(object sender, EventArgs e)
+        {textBox6.Text = "номер 6";
+         textBox6.BackColor = System.Drawing.Color.Gold;}
+private void textBox7_Click(object sender, EventArgs e)
+        {textBox7.Text = "номер 7";
+         textBox7.BackColor = System.Drawing.Color.Gold;}
+private void textBox8_Click(object sender, EventArgs e)
+        {textBox8.Text = "номер 8";
+         textBox8.BackColor = System.Drawing.Color.Gold;}
+private void textBox9_Click(object sender, EventArgs e)
+        {textBox9.Text = "номер 9";
+         textBox9.BackColor = System.Drawing.Color.Gold;}
+
+
+
+
+
+
+private void Form1_Resize(object sender, EventArgs e)
+        {Control control = (Control)sender;
+ if (control.Size.Height != control.Size.Width)
+            { control.Size = new Size(control.Size.Width, control.Size.Width);
+                textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = "";
+                textBox4.Text = ""; textBox5.Text = ""; textBox6.Text = "";
+                textBox7.Text = ""; textBox8.Text = ""; textBox9.Text = "";
+                textBox1.BackColor = System.Drawing.Color.White;
+                textBox2.BackColor = System.Drawing.Color.White;
+                textBox3.BackColor = System.Drawing.Color.White;
+                textBox4.BackColor = System.Drawing.Color.White;
+                textBox5.BackColor = System.Drawing.Color.White;
+                textBox6.BackColor = System.Drawing.Color.White;
+                textBox7.BackColor = System.Drawing.Color.White;
+                textBox8.BackColor = System.Drawing.Color.White;
+                textBox9.BackColor = System.Drawing.Color.White;
+                textBox10.Text = "";
+                textBox10.BackColor = System.Drawing.Color.White;}}
+
+
+
+private void textBox10_Click(object sender, EventArgs e)
+        {textBox10.Text = "Dana";
+         textBox10.BackColor = System.Drawing.Color.Green;}}}
